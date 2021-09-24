fccb06567bb3a0f457eb13721439bd55952aabb9
Calvin Koepke <hello@calvinkoepke.com>
2021-09-24 00:10:59 -0600

    fix: clean it out

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,4 @@
-const resolveConfig = require("tailwindcss/resolveConfig");
 const tailwindConfig = require("./tailwind.config.js");
-
-const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
