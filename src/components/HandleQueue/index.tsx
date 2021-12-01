b332300d22151610d08ebbe437b4305e2a5ae6bc
Calvin Koepke <2815568+cjkoepke@users.noreply.github.com>
2021-12-01 14:52:03 -0700

    chore: add spam notice

--- src/components/HandleQueue/index.tsx
+++ src/components/HandleQueue/index.tsx
@@ -251,7 +251,8 @@ export const HandleQueue = (): JSX.Element => {
             Success!
           </h3>
           <p className="text-lg text-center text-dark-350">{responseMessage}</p>
-          <p className="text-center text-lg font-bold">You may close this window.</p>
+          <p>Make sure to <strong className="underline">add adahandle.com to your safe-senders list, as well as check your spam folder!</strong></p>
+          <p className="text-center text-lg font-bold">You may close this window!</p>
         </div>
       ) : (
         <>
