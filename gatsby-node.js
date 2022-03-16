6fb214b562efff9bad2bdb475275e39472087cf3
guavajellyaaron <aaron@guavajellyco.com>
2022-03-15 17:53:02 -0700

    add version to files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,13 @@
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   actions.setWebpackConfig({
-      experiments: {
-          syncWebAssembly: true,
-      },
+    experiments: {
+      syncWebAssembly: true,
+    },
+    output: {
+      filename: `[name].js?v=2`,
+      chunkFilename: `[name].js?v=2`,
+      path: getConfig().output.path,
+      publicPath: getConfig().output.publicPath,
+    },
   });
 };
