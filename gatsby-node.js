06483b0d2b649d23a3bf0982a710e67710fc96a0
guavajellyaaron <aaron@guavajellyco.com>
2022-03-15 18:24:04 -0700

    add cache buster

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,44 @@
-exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+const md5 = require("md5");
+const path = require("path");
+const util = require("util");
+const glob = require("glob");
+const fs = require("fs");
+
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     experiments: {
       syncWebAssembly: true,
     },
-    output: {
-      filename: `[name].js?v=2`,
-      chunkFilename: `[name].js?v=2`,
-      path: getConfig().output.path,
-      publicPath: getConfig().output.publicPath,
-    },
   });
+};
+
+const hash = md5(`${new Date().getTime()}`);
+
+const addPageDataVersion = async (file) => {
+  const stats = await util.promisify(fs.stat)(file);
+  if (stats.isFile()) {
+    console.log(`Adding version to page-data.json in ${file}..`);
+    let content = await util.promisify(fs.readFile)(file, "utf8");
+    const result = content.replace(
+      /page-data.json(\?v=[a-f0-9]{32})?/g,
+      `page-data.json?v=${hash}`
+    );
+    await util.promisify(fs.writeFile)(file, result, "utf8");
+  }
+};
+
+exports.onPostBootstrap = async () => {
+  const loader = path.join(
+    __dirname,
+    "node_modules/gatsby/cache-dir/loader.js"
+  );
+  await addPageDataVersion(loader);
+};
+
+exports.onPostBuild = async () => {
+  const publicPath = path.join(__dirname, "public");
+  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`);
+  for (let file of htmlAndJSFiles) {
+    await addPageDataVersion(file);
+  }
 };
