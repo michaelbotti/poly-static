3306217eb7b4169049abf5f3b62d922fecb87795
guavajellyaaron <aaron@guavajellyco.com>
2022-03-22 22:00:43 -0700

    bump version

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -57,7 +57,7 @@ const Layout: FC = ({ children }): JSX.Element => {
             >
               {policyID}
             </a>{" "}
-            • Version: 1.2.0
+            • Version: 1.2.1
           </p>
         </footer>
       </div>
